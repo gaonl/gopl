212a092ceff6cd4c0436eaa69668a1d0d2b5f3c4
gaonl <gnlyjy@163.com>
2019-08-29 10:35:55 +0800

    readmd.md & reflect demo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gopl
-go lang study
+## 内存模型  happens-before
+1. 初始化  init()函数
+2. 创建goroutine(注意：销毁goroutine并不提供保证)
+3. 通道（区分带缓冲和不带缓冲）
+4. 锁
+5. 一次执行（once.Do(setup)）
+[详细的，请请点击我](https://blog.csdn.net/zhailuxu/article/details/88085864)
---
+++ reflecttest/reflectdemo.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
+type Car struct {
+	Color byte `json:"color"`
+	Price int  `json:"price"`
+}
+
+func reflectStruct() {
+	var c Car
+	t := reflect.TypeOf(c)
+	field, ok := t.FieldByName("Color")
+	if ok {
+		fmt.Println(field.Name)
+		fmt.Println(field.Tag)
+	}
+
+
+	v:=reflect.ValueOf(c)
+	value := v.FieldByName("Colors")
+	fmt.Println(value.IsValid())
+}
+
+func reflecttest() {
+	var i int
+	println("1: ", reflect.ValueOf(i).Kind().String()) // Output: int
+	println("2: ", reflect.TypeOf(i).Kind().String())  // Output: int
+
+	var pi *int
+	pi = &i
+	println("3: ", reflect.ValueOf(pi).Elem().CanSet()) // Output: true   Value.Elem 和 Type.Elem()不一样
+	println("4: ", reflect.TypeOf(pi).Elem().String())  // Output: int    Type.Elem() 指的是数据指向类型
+
+	var s string
+	println("5: ", reflect.ValueOf(s).Kind().String()) // Output: string
+
+	var iface interface{}
+	println("6.1: ", reflect.ValueOf(iface).IsValid()) //Output: false //标记1
+	println("6: ", reflect.ValueOf(iface).Kind().String()) //Output: invalid //标记1
+	//println("7: ", reflect.TypeOf(iface).Kind().String())                 //panic.根本没有type //标记1
+	println("8: ", reflect.ValueOf(&iface).Elem().Kind().String())        //Output: interface // 标记2
+	println("9: ", reflect.ValueOf(&iface).Elem().Elem().Kind().String()) //Output: invalid // 标记3
+
+	var ifaceNotNil interface{} = "wskj"
+	println("10: ", reflect.ValueOf(ifaceNotNil).Kind().String())                //Output: string //标记1
+	println("11: ", reflect.TypeOf(ifaceNotNil).Kind().String())                 //Output: string //标记1
+	println("12: ", reflect.ValueOf(&ifaceNotNil).Elem().Kind().String())        //Output: interface // 标记2
+	println("13: ", reflect.ValueOf(&ifaceNotNil).Elem().Elem().Kind().String()) //Output: string // 标记3
+
+	var t time.Time
+	println(reflect.ValueOf(t).Kind().String()) //Output: struct //标记4
+}
+
+func main() {
+	reflecttest()
+	//reflectStruct()
+}
